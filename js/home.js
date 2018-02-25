ebaeb2d7ae1540d1320a6e1e10b41b26b94f2aa9
Ryan Kleeberger <ryanklee@gmail.com>
2018-02-25 12:30:07 -0600

    moved everything into document ready

--- js/home.js
+++ js/home.js
@@ -37,54 +37,56 @@ $(document).ready(function () {
       }
     })
   });
+
+  function loadDVDList() {
+    var DVDTableRows = $('#DVDTableRows');
+  
+    $.ajax({
+      type: 'GET',
+      url: 'http://localhost:8080/dvds',
+      success: function (DVDArray) {
+        $.each(DVDArray, function (index, dvd) {
+  
+          var title = dvd.title;
+          var releaseYear = dvd.realeaseYear;
+          var director = dvd.director;
+          var rating = dvd.rating;
+  
+          var row = '<tr>';
+          row += '<td>' + title + '</td>';
+          row += '<td>' + releaseYear + '</td>';
+          row += '<td>' + director + '</td>';
+          row += '<td>' + rating + '</td>';
+          row += '<td><a>Edit</a> | <a>Delete</a></td>';
+          row += '</tr>';
+  
+          DVDTableRows.append(row);
+        });
+      },
+      error: function () {
+        $('#errorMessages')
+          .append($('<li>')
+            .attr({ class: 'list-group-item list-group-item-danger' })
+            .text('Error calling web service.'));
+      }
+    });
+  }
+  
+  function hideDVDCreate() {
+    $("#createDVDForms").hide();
+    showDVDTable();
+    $("#header").show();
+  }
+  
+  function hideDVDTable() {
+    $("#DVDTableDiv").hide();
+    $("#header").hide();
+    $("#createDVDForms").show();
+  }
+  
+  function showDVDTable() {
+    $("#DVDTableDiv").show();
+  }
+  
 });
 
-function loadDVDList() {
-  var DVDTableRows = $('#DVDTableRows');
-
-  $.ajax({
-    type: 'GET',
-    url: 'http://localhost:8080/dvds',
-    success: function (DVDArray) {
-      $.each(DVDArray, function (index, dvd) {
-
-        var title = dvd.title;
-        var releaseYear = dvd.realeaseYear;
-        var director = dvd.director;
-        var rating = dvd.rating;
-
-        var row = '<tr>';
-        row += '<td>' + title + '</td>';
-        row += '<td>' + releaseYear + '</td>';
-        row += '<td>' + director + '</td>';
-        row += '<td>' + rating + '</td>';
-        row += '<td><a>Edit</a> | <a>Delete</a></td>';
-        row += '</tr>';
-
-        DVDTableRows.append(row);
-      });
-    },
-    error: function () {
-      $('#errorMessages')
-        .append($('<li>')
-          .attr({ class: 'list-group-item list-group-item-danger' })
-          .text('Error calling web service.'));
-    }
-  });
-}
-
-function hideDVDCreate() {
-  $("#createDVDForms").hide();
-  showDVDTable();
-  $("#header").show();
-}
-
-function hideDVDTable() {
-  $("#DVDTableDiv").hide();
-  $("#header").hide();
-  $("#createDVDForms").show();
-}
-
-function showDVDTable() {
-  $("#DVDTableDiv").show();
-}
